2d32ace7fd7dceac11832ae3ba8d8d64a4ccb5a3
naga12334 <154617811+naga12334@users.noreply.github.com>
2025-03-31 16:26:22 +0530

    v

--- main.go
+++ main.go
@@ -6,9 +6,10 @@ import (
 
 	"github.com/Phaneendraaa/loadbalancer-go/serverHandler"
 )
+
 func main() {
 	fmt.Println("Started load balancer")
 	http.HandleFunc("/", serverHandler.ProxyHandler)
-	
-	http.ListenAndServe(":8080", nil)
+
+	http.ListenAndServe(":8082", nil)
 }
