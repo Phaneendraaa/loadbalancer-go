ffa253409f227ef1a76e24169dc2127d06d39ccb
naga12334 <154617811+naga12334@users.noreply.github.com>
2025-03-31 14:43:53 +0530

    added reverse proxy

--- main.go
+++ main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httputil"
 	"net/url"
+	"sync"
 )
 
+var currBackend int
+var mu sync.Mutex
 var backendServers = []*url.URL{
 	{Scheme: "http", Host: "3.84.189.215"},
 	{Scheme: "http", Host: "34.238.251.205"},
 	{Scheme: "http", Host: "18.206.250.71"},
 }
 
+func getNextBackend() *url.URL {
+	mu.Lock()
+	defer mu.Unlock()
+	backend := backendServers[currBackend]
+	currBackend = (currBackend + 1) % len(backendServers)
+	return backend
+}
+func proxyHandler(w http.ResponseWriter, r *http.Request) {
+	backendserver := getNextBackend()
+	proxy := httputil.NewSingleHostReverseProxy(backendserver)
+	proxy.ServeHTTP(w, r)
+}
 func main() {
-	fmt.Println(backendServers)
+	fmt.Println("Started load balancer")
+	http.HandleFunc("/", proxyHandler)
+	http.ListenAndServe(":80", nil)
 }
